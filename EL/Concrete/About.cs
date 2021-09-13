4a4d178c7a011f2ba00038fcde0270eca7a08410
Emre <eemmrree0@outlook.com>
2021-09-13 15:35:10 +0300

    EntityLayer katmanında Concrete klasöründe About propertyleri oluşturuldu.

---
+++ EL/Concrete/About.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL.Concrete
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public int AboutDetail1 { get; set; }
+        public int AboutDetail2 { get; set; }
+        public int AboutImage1 { get; set; }
+        public int AboutImage2 { get; set; }
+        public int AboutMapLocation { get; set; }
+        public bool AboutStatus { get; set; }
+    }
+}
