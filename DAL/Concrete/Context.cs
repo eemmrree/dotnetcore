f585dc9b0c1828bfb9d4a9b1ee005540bfe352c2
Emre <eemmrree0@outlook.com>
2021-09-13 16:00:22 +0300

    DataAccessLayer katmanında Concrete klasöründe Context sınıfında Sql bağlantısı yapıldı.

---
+++ DAL/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-H6JU5SG\\SQLEXPRESS; database=CoreBlogDb; integrated security=true;");
+        }
+    }
+}
