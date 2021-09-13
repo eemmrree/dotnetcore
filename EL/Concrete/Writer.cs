5402d0c31118a0520ecb3133e139a62202258404
Emre <eemmrree0@outlook.com>
2021-09-13 15:35:57 +0300

    EntityLayer katmanında Concrete klasöründe Writer propertyleri oluşturuldu.

---
+++ EL/Concrete/Writer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL.Concrete
+{
+    public class Writer
+    {
+        public int WriterID { get; set; }
+        public int WriterName { get; set; }
+        public int WriterAbout { get; set; }
+        public int WriterImage { get; set; }
+        public int WriterMail { get; set; }
+        public int WriterPassword { get; set; }
+        public int WriterStatus { get; set; }
+    }
+}
