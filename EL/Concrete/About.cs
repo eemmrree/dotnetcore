1d7e97e18f55bac09c30ff737df9d3a9a8802c27
Emre <eemmrree0@outlook.com>
2021-09-13 16:01:52 +0300

    EntityLayer katmanında propertylere [Key] attribute eklendi

--- EL/Concrete/About.cs
+++ EL/Concrete/About.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EL.Concrete
 {
     public class About
     {
+        [Key]
         public int AboutID { get; set; }
         public int AboutDetail1 { get; set; }
         public int AboutDetail2 { get; set; }
