80dcc7c35a81100135a497f1b405a37a7e25facc
Emre <eemmrree0@outlook.com>
2021-10-05 21:38:15 +0300

    Yazara ait son bloglar listeleme işlemi.

---
+++ DotNetCore/ViewComponents/Blog/WriterLastBlogs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Concrete;
+using DAL.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.ViewComponents.Blog
+{
+    public class WriterLastBlogs:ViewComponent
+    {
+         BlogManager bm = new BlogManager(new EfBlogRepository());
+
+         public IViewComponentResult Invoke()
+         {
+             var values = bm.GetBlogListByWriter(3);
+             return View(values);
+         }
+    }
+}
