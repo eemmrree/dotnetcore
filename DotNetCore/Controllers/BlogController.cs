6ee7940484d9b5f12be85a6cde5eef4c96c789b2
Emre <eemmrree0@outlook.com>
2021-10-05 14:53:31 +0300

    updated

---
+++ DotNetCore/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Concrete;
+using DAL.Concrete.EntityFramework;
+
+namespace DotNetCore.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+           var values = bm.GetBlogList();
+            return View(values);
+        }
+
+        public IActionResult BlogReadAll(int id)
+        {
+            ViewBag.i = id;
+            var values = bm.GetBlogById(id);
+            return View(values);
+        }
+    }
+}
---
+++ DotNetCore/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Concrete;
+using DAL.Concrete.EntityFramework;
+
+namespace DotNetCore.Controllers
+{
+    public class CommentController : Controller
+    {
+        private CommentManager cm = new CommentManager(new EfCommentRepository());
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult CommentListByBlog(int id)
+        {
+            
+           var values=cm.GetList(id);
+            return PartialView(values);
+        }
+    }
+}
