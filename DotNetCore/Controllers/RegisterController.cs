1d0c176f055c3a11da0f648d6cff1f55c6aacea5
Emre <eemmrree0@outlook.com>
2021-10-05 18:15:31 +0300

    Kayıt Olma işlemi için RegisterController oluşturuldu.

---
+++ DotNetCore/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Concrete;
+using DAL.Concrete.EntityFramework;
+using EL.Concrete;
+
+namespace DotNetCore.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private WriterManager wm = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost] 
+        public IActionResult Index(Writer p)
+        {
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
