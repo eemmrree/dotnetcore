89dd77ffefe81f34e3d1218aed015240c24da91e
Emre <eemmrree0@outlook.com>
2021-10-05 14:54:46 +0300

    ViewComponent sınıfları oluşturuldu.

---
+++ DotNetCore/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Concrete;
+using DAL.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.ViewComponents.Category
+{
+    public class CategoryList : ViewComponent
+    {
+        private CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
