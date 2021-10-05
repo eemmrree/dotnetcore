c62caf16bf45207e3cd574b75ba7aec1384bbd70
Emre <eemmrree0@outlook.com>
2021-10-05 21:40:52 +0300

    Kayıt olma sayfasında değerlerini view modelde tutucağımız bir şehir seçimi ve Parolanın 2 kez girilip onaylanması.

---
+++ DotNetCore/Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EL.Concrete;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DotNetCore.Models
+{
+    public class City
+    {
+
+        public string CityName { get; set; }
+
+
+    }
+}
+
---
+++ DotNetCore/Models/WriterCity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EL.Concrete;
+
+namespace DotNetCore.Models
+{
+    public class WriterCity
+    {
+        public List<City> city { get; set; }
+        public List<Writer> writers { get; set; }
+    }
+}
