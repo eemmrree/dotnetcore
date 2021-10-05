4188926800d643bbb364e435b2a3b626e065762a
Emre <eemmrree0@outlook.com>
2021-10-05 14:51:15 +0300

    BLL katmanında interface'ler oluşturuldu

---
+++ BLL/Abstract/IBlogService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL.Concrete;
+
+namespace BLL.Abstract
+{
+    public interface IBlogService
+    {
+        void BlogAdd(Blog blog);
+        void BlogDelete(Blog blog);
+        void BlogUpdate(Blog blog);
+        List<Blog> GetList();
+        Blog GetById(int id);
+
+        List<Blog> GetBlogList();
+
+        
+    }
+}
---
+++ BLL/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL.Concrete;
+
+namespace BLL.Abstract
+{
+    public interface ICommentService
+    {
+        void CommentAdd(Comment comment);
+        //void CommentDelete(Comment comment);
+        //void CommentUpdate(Comment comment);
+        List<Comment> GetList(int id);
+        //Comment GetById(int id);
+    }
+}
