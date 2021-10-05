72b44fed0552e2c12a8de6b914de0c4eab40f87e
Emre <eemmrree0@outlook.com>
2021-10-05 18:14:42 +0300

    BLL katmanında düzenlemeler yapıldı.

---
+++ BLL/Concrete/WriterManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Abstract;
+using DAL.Abstract;
+using EL.Concrete;
+
+namespace BLL.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+        private IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+        public void WriterAdd(Writer writer)
+        {
+           _writerDal.Insert(writer);
+        }
+    }
+}
