99fb6f501b72edda2350b1dc4be868e87244caf5
Emre <eemmrree0@outlook.com>
2021-10-05 14:53:08 +0300

    updated

--- DAL/Concrete/EntityFramework/EfBlogRepository.cs
+++ DAL/Concrete/EntityFramework/EfBlogRepository.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 using DAL.Abstract;
 using DAL.Concrete.Repositories;
 using EL.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Concrete.EntityFramework
 {
     public class EfBlogRepository:GenericRepository<Blog> , IBlogDal
     {
+        public List<Blog> getListWithCategory()
+        {
+            using (var c = new Context())
+            {
+                return c.Blogs.Include(x => x.Category).ToList();
+            }
+           
+        }
     }
 }
