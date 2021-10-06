f5c3e95c6e4dfd00034bd03dde76020b28d1eb85
Emre <eemmrree0@outlook.com>
2021-10-06 22:26:21 +0300

    ContactManager sınıfı oluşturuldu.

---
+++ BLL/Concrete/ContactManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Abstract;
+using DAL.Abstract;
+using EL.Concrete;
+
+namespace BLL.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+        public void ContactAdd(Contact contact)
+        {
+           _contactDal.Insert(contact);
+        }
+    }
+}
