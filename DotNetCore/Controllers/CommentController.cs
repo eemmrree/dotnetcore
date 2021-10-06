fe4675b838bdddfdf8d16ffc35855301847d70b1
Emre <eemmrree0@outlook.com>
2021-10-06 20:30:12 +0300

    Controllerda düzenlemeler yapıldı.

--- DotNetCore/Controllers/CommentController.cs
+++ DotNetCore/Controllers/CommentController.cs
@@ -5,15 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using BLL.Concrete;
 using DAL.Concrete.EntityFramework;
+using EL.Concrete;
 
 namespace DotNetCore.Controllers
 {
     public class CommentController : Controller
     {
-        private CommentManager cm = new CommentManager(new EfCommentRepository());
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+
+        [HttpGet]
         public PartialViewResult PartialAddComment()
         {
             return PartialView();
+        }
+        [HttpPost]
+        public IActionResult PartialAddComment(Comment p)
+        {
+            p.CommentDate=DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            p.BlogID = 2; 
+            cm.CommentAdd(p);
+            return RedirectToAction("Index", "Blog");
         }
 
         public PartialViewResult CommentListByBlog(int id)
---
+++ DotNetCore/Controllers/NewsLetterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Concrete;
+using DAL.Concrete.EntityFramework;
+using EL.Concrete;
+
+namespace DotNetCore.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        NewsLetterManager nl = new NewsLetterManager(new EfNewsLetterRepository());
+
+
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult SubscribeMail(NewsLetter p)
+        {
+            p.MailStatus = true;
+            nl.AddNewsLetter(p);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
