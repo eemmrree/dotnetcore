f657d904a53921fba196f06fa4606a6eb898afc3
Emre <eemmrree0@outlook.com>
2021-10-06 20:27:11 +0300

    Yorum ve Abone bülteni bülteni için BLL katmanında maganer sınıfları oluşturuldu.

---
+++ BLL/Concrete/NewsLetterManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Abstract;
+using DAL.Abstract;
+using EL.Concrete;
+
+namespace BLL.Concrete
+{
+    public class NewsLetterManager : INewsLetterService
+    {
+        INewsLetterDal _newsLetter;
+
+        public NewsLetterManager(INewsLetterDal newsLetter)
+        {
+            _newsLetter = newsLetter;
+        }
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newsLetter.Insert(newsLetter);
+        }
+    }
+}
