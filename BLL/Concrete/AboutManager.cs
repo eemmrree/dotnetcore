af809448badaf36315384f0496a107e51d3304de
Emre <eemmrree0@outlook.com>
2021-10-06 21:28:28 +0300

    BLL katmanında About service ve manager sınıf,interface oluşturuldu

---
+++ BLL/Concrete/AboutManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Abstract;
+using DAL.Abstract;
+using EL.Concrete;
+
+namespace BLL.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public List<About> GetList()
+        {
+            return _aboutDal.GetListAll();
+        }
+    }
+}
