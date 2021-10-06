0afdd2c3c3ec417fad60035bfea4faaaf9df273e
Emre <eemmrree0@outlook.com>
2021-10-06 20:28:45 +0300

    DAL katmanında Mail bültenine abone ol interface ve Ef sınıfları oluşturuldu.

---
+++ DAL/Abstract/INewsLetterDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL.Concrete;
+
+namespace DAL.Abstract
+{
+    public interface INewsLetterDal : IGenericDal<NewsLetter>
+    {
+    }
+}
