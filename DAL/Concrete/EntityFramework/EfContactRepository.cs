d0dced3ee1817fbdda449494c64743c4ca5ea4ee
Emre <eemmrree0@outlook.com>
2021-10-06 22:27:16 +0300

    updated

--- DAL/Concrete/EntityFramework/EfContactRepository.cs
+++ DAL/Concrete/EntityFramework/EfContactRepository.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Abstract;
 using DAL.Concrete.Repositories;
 using EL.Concrete;
 
 namespace DAL.Concrete.EntityFramework
 {
-    public class EfContactRepository:GenericRepository<Contact>
+    public class EfContactRepository:GenericRepository<Contact> , IContactDal
     {
     }
 }
---
+++ DAL/Migrations/20211006190239_mig5.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class mig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactDate",
+                table: "Contacts",
+                newName: "ContactDate2");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ContactStatus2",
+                table: "Contacts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactStatus2",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "ContactDate2",
+                table: "Contacts",
+                newName: "ContactDate");
+        }
+    }
+}
