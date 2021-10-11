115e21f5d88f0477c03969e20aa5e668422cf120
Emre <eemmrree0@outlook.com>
2021-10-11 13:03:17 +0300

    Startup.Cs Configure Service de Proje Seviyesinde Authorize işlemi yapıldı.

--- DotNetCore/Startup.cs
+++ DotNetCore/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace DotNetCore
 {
@@ -24,6 +26,13 @@ namespace DotNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                config.Filters.Add(new AuthorizeFilter(policy)); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
