d067561a7fbb9876ef3c21fb80b13c3e02c6fc37
Emre <eemmrree0@outlook.com>
2021-10-31 13:46:15 +0300

    DAL katmanında EfBlogRepository düzenlendi

--- DAL/Concrete/EntityFramework/EfBlogRepository.cs
+++ DAL/Concrete/EntityFramework/EfBlogRepository.cs
@@ -12,13 +12,16 @@ namespace DAL.Concrete.EntityFramework
 {
     public class EfBlogRepository:GenericRepository<Blog> , IBlogDal
     {
-        public List<Blog> getListWithCategory()
+        public List<Blog> GetListWithCategory()
         {
-            using (var c = new Context())
-            {
-                return c.Blogs.Include(x => x.Category).ToList();
-            }
-           
+            using var c = new Context();
+            return c.Blogs.Include(x => x.Category).ToList();
+        }
+
+        public List<Blog> GetListWithCategoryByWriter(int id)
+        {
+            using var c = new Context();
+            return c.Blogs.Include(x => x.Category).Where(x=>x.WriterID==id).ToList();
         }
     }
 }
